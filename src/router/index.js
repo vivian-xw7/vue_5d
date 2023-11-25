bdbee07bdec66c6506be1116f15e1f2e59e15815
Vivian Hayes <vivian.xw7@gmail.com>
2023-11-25 13:31:23 -0600

    error handling

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import EventRegister from '../views/event/Register.vue'
 import EventEdit from '../views/event/Edit.vue'
 import EventLayout from '../views/event/Layout.vue'
 import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
+import NetworkError from '../views/NetworkError.vue'
 
 const routes = [
   {
@@ -46,6 +48,22 @@ const routes = [
     redirect: to => {
       return { path: '/events/' + to.params.afterEvent }
     }
+  },
+  {
+    path: '/:catchAll(.*)',
+    Name: 'NotFound',
+    component: NotFound
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError
   }
 ]
 
---
+++ src/views/NetworkError.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Oops!</h1>
+  <h3>It looks like you're experiencing some network issues. Please <a href="#" @click="$router.go(-1)">click here</a> to try again.</h3>
+</template>
+
+<!-- <script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true,
+      default: 'page'
+    }
+  }
+}
+</script> -->
