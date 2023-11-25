4730530a94c2ce5f210dcd5807d2d7526a2ee91b
Vivian Hayes <vivian.xw7@gmail.com>
2023-11-25 12:55:49 -0600

    programatic navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,11 +1,18 @@
 <template>
   <div v-if="event">
     <p>Register for event</p>
+    <button @click="register">Register Me</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      this.$router.push({
+        name: 'EventDetails'      })
+    }
+  }
 }
 </script>
