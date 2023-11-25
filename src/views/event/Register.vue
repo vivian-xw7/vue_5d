96836b44b697fe926cb16118605df4483cd6309b
Vivian Hayes <vivian.xw7@gmail.com>
2023-11-25 13:52:53 -0600

    flash message

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -8,10 +8,19 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
+      this.GStore.flashMessage =
+        'You are successfully registered for this ' + this.event.title
+
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+
       this.$router.push({
-        name: 'EventDetails'      })
+        name: 'EventDetails'
+      })
     }
   }
 }
